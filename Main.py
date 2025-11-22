e5b2738c1d4b04d01da8ee71062e03ab790c54e4
Franklin <mwangifranklin541@gmail.com>
2025-11-23 00:44:44 +0300

    Refactor Main.py to improve structure and imports

--- Main.py
+++ Main.py
@@ -1,12 +1,55 @@
-import.os
-import.datetime
-  class assistant:
-    """ a small extendable assistant
-        -'uses a sample rule based response
-    """
-def respond (self,text:str)->str
-    """ return short response for given text
-    """
-  text (text or ""). strip()
-  if not text:
-    return " please say something so that I can help!"
+import json
+from difflib import get_close_match
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if name_='_main_':
+ chatbot()
+
+
+
