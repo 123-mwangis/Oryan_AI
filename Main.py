5269e15dadc56c3eb24d879dec296755f4d868f7
Franklin <mwangifranklin541@gmail.com>
2025-11-23 09:23:34 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import json
 from difflib import get_close_match
 
+def get_knowledge_base (file_path:str)->dict:
+  with open (file_path, 'r') as file:
+   data:json. load(file)
+  return data
+
+def save_knowledge_base (file_path : str,data:dict
+
 
 
 
